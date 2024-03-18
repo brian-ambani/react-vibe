2786a411b6194107bbd29e3bdbdbea8b77b6ca5d
brian-ambani <brainambani1@gmail.com>
2024-03-18 13:57:47 +0300

    add create project, get all projects and  get a specific project controllers

--- server/routes/projects.routes.js
+++ server/routes/projects.routes.js
@@ -1,34 +1,20 @@
 const express = require("express");
-const Project = require("../models/projectModel");
+const {
+  createProject,
+  getProjects,
+  getProject,
+} = require("../controllers/projectController");
 
 const router = express.Router();
 
 // GET all projects
-router.get("/", (req, res) => {
-  res.json({ message: "GET all projects" });
-});
+router.get("/", getProjects);
 
 //GET a single project
-router.get("/:id", (req, res) => {
-  res.json({ message: "GET a single project" });
-});
+router.get("/:id", getProject);
 
 // POST a new project
-router.post("/", async (req, res) => {
-  const { title, description, status, dueDate } = req.body;
-
-  try {
-    const project = await Project.create({
-      title,
-      description,
-      status,
-      dueDate,
-    });
-    res.status(200).json(project);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+router.post("/", createProject);
 
 // DELETE a new project
 router.delete("/:id", (req, res) => {
