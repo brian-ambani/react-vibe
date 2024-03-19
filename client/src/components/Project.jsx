5aed4f15c755ed8e18cb10ca950dd7f0633dc5ee
brian-ambani <brainambani1@gmail.com>
2024-03-19 14:28:35 +0300

    add projectcard

--- client/src/components/Project.jsx
+++ client/src/components/Project.jsx
@@ -1,11 +1,13 @@
 const Project = ({ title, description }) => {
   return (
-    <div className="w-[250px] h-[197px] border rounded-md mt-8 p-4">
-      <h2 className="text-[#1E293B] font-bold mb-1">UX Adjustment</h2>
+    <div className="w-[250px] h-[197px] border rounded-md mt-8 p-4 cursor-pointer">
+      <h2 className="text-[#1E293B] font-bold mb-1 ">UX Adjustment</h2>
       <p className="text-[#94A3B8] mb-2">
         It just needs to adapt the UI from what you did before
       </p>
-      <h4 className="mb-2 bg-[#F0FAFF] text-[#33BFFF] ">Research</h4>
+      <h4 className="mb-2 bg-[#F0FAFF] text-[#33BFFF] h-8 w-20 flex items-center justify-center rounded-md">
+        Research
+      </h4>
       <div className="flex justify-between mt-2 border-t-2 pt-5">
         <p>8</p>
         <p>Tomorrow</p>
